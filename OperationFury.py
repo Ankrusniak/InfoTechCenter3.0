6f77f67081622d88f4be4142f62c913f0ec02171
Andrew Krusniak <andrew.krusniakjr@gmail.com>
2022-10-05 08:25:23 -0400

    addd 5 loops to os

--- OperationFury.py
+++ OperationFury.py
@@ -11,8 +11,15 @@ drivers know that the InfoTechCenter OS is Loading
 
 from time import sleep #We imported the sleep function from the Time library
 
+print("\033[1;33m This text is Dark Blue \n")
 
-sleep(2)
+
 print("\n\nWelcome to Operation Fury InfoTech Center")
-
+sleep(2)
 print("\nOperation Fury's Operation system is Booting Up")
+
+
+for i in range(2):
+print("Os Booting up"sleep(5)
+print("\033{1;36m os Booting up")
+sleep(5) 
