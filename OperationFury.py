ad30bcdb27f5838f4f3010d7f7dfe2d3058ea562
Andrew Krusniak <andrew.krusniakjr@gmail.com>
2022-10-05 08:40:27 -0400

    Inisale weather cominte

---
+++ OperationFury.py
@@ -0,0 +1,10 @@
+Weather
+#Developer:Andrew Krusniak
+#version 1.0
+
+"""
+Create a function for our current weather using the 
+random.choice function to determine what the weather is 
+picking from a list-using a If,elif & E;se statements 
+to check the condition and print a specific print line
+""" 
