d618a41941250e93a6f18bc9a89b13ff870ba424
Andrew Krusniak <andrew.krusniakjr@gmail.com>
2022-10-06 08:38:15 -0400

    Defined the weather function

--- OperationFury.py
+++ OperationFury.py
@@ -1,4 +1,4 @@
-Weather
+#Weather
 #Developer:Andrew Krusniak
 #version 1.0
 
@@ -7,4 +7,14 @@ Create a function for our current weather using the
 random.choice function to determine what the weather is 
 picking from a list-using a If,elif & E;se statements 
 to check the condition and print a specific print line
-""" 
+"""
+
+#Import Libraries here
+import random
+#Weather condition list using the random.choic library
+#to randomly choos a condition and storing it in its brain
+def weather():
+    weather_forcast = ["Rain","Snow","Sunny","Cloudy","Foggy","Storming","Icy"]
+    weatherCondition = random.choice(weather_forcast)
+    return weatherCondition
+
